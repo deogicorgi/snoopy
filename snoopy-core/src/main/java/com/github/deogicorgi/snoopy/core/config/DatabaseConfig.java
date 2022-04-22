89726df6c7e4e03547bade5bf078d06d1ef29abc
deogicorgi <deogicorgi@gmail.com>
2022-04-22 21:43:02 +0900

    프로젝트 구조 변경

--- snoopy-core/src/main/java/com/github/deogicorgi/snoopy/core/config/DatabaseConfig.java
+++ snoopy-core/src/main/java/com/github/deogicorgi/snoopy/core/config/DatabaseConfig.java
@@ -1,10 +1,10 @@
-package com.github.deogicorgi.snoopy.orm.config;
+package com.github.deogicorgi.snoopy.core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.github.deogicorgi.snoopy.orm.repository")
+@EnableJpaRepositories(basePackages = "com.github.deogicorgi.snoopy.core.orm.repository")
 public class DatabaseConfig {
 
     public DatabaseConfig(){
